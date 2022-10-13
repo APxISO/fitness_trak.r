5aaba95331d169661c71c34cd95708b204f0e0c8
Adam Perez <amperez0208@gmail.com>
2022-10-13 17:09:46 -0400

    fixed header positioning

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,17 +4,17 @@ import './header.css'
 function Header() {
 return (
     <nav>
-<div class="container nav__container">
-            <a href="" class="nav__logo">EGATOR</a>
-            <ul class="nav__items">
+<div className="nav__container">
+            <a href="" className="nav__logo">Fitness Trak.r</a>
+            <ul className="nav__items">
                 <li><a href="">Home</a></li>
                 <li><a href="">Routines</a></li>
                 <li><a href="">Activities</a></li>
                 <li><a href="">Logout</a></li>        
             </ul>
 
-            <button id="open__nav-btn"><i class="uil uil-bars"></i></button>
-            <button id="close__nav-btn"><i class="uil uil-multiply"></i></button>
+            <button id="open__nav-btn"><i className="uil uil-bars"></i></button>
+            <button id="close__nav-btn"><i className="uil uil-multiply"></i></button>
         </div>
         </nav>
 )
