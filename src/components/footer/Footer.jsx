6ac87f0bb2b7c5ff788b778223e40a0fed73709e
Adam Perez <amperez0208@gmail.com>
2022-10-25 15:08:30 -0400

    fixed footer styling

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,14 +7,13 @@ import {BsInstagram} from 'react-icons/bs'
 
 function Footer() {
 return (
-    <footer>
+    <footer className='footer__container'>
+        
     <div className="footer__socials">
         <a href="https://youtube.com" target="_blank"><i><BsYoutube /></i></a>
         <a href="https://facebook.com" target="_blank"><i><BsFacebook/></i></a>
         <a href="https://instagram.com" target="_blank"><i><BsInstagram/></i></a>
     </div>
-    
-    
     <div className="footer__copyright">
         <small>Copyright &copy; 2022 by FS Team. All rights reversed</small>
     </div>
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './home.css'
+
+const Home = ({user}) => {
+    return (
+        <>
+          <div class="home_container">
+  <div class="home_content">
+    <h1>Pratham</h1>
+    <h3>I love designing websites and keep things as simple as possible. My goals is to focus on minimalism and conveying the message that you want to send</h3>
+  </div>
+  </div>
+        </>
+      );
+}
+
+
+
+
+
+export default Home
