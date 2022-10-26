ec33ed46704d14be3225be1b18a66c1cb50208ce
Adam Perez <amperez0208@gmail.com>
2022-10-26 17:12:11 -0400

    created home component with mild styling

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -4,10 +4,15 @@ import './home.css'
 const Home = ({user}) => {
     return (
         <>
-          <div class="home_container">
-  <div class="home_content">
-    <h1>Pratham</h1>
-    <h3>I love designing websites and keep things as simple as possible. My goals is to focus on minimalism and conveying the message that you want to send</h3>
+          <div className="home_card">
+  <div className="home_content">
+    <h1>Welcome to Fitness Trackr</h1>
+    <h3>If you are a new user, set up your account below</h3>
+    <button>Register</button>
+  
+  
+  
+  
   </div>
   </div>
         </>
