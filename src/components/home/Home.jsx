ff0f11fb5d09b8245882ee3a13d513f93ad13d9c
Adam Perez <amperez0208@gmail.com>
2022-10-26 17:54:19 -0400

    home component built and basic styling

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import './home.css'
+// import {Link} from 'react-router-dom';
 
 const Home = ({user}) => {
     return (
         <>
           <div className="home_card">
   <div className="home_content">
-    <h1>Welcome to Fitness Trackr</h1>
-    <h3>If you are a new user, set up your account below</h3>
-    <button>Register</button>
-  
-  
+    <div className='home_card_options'>
+        <h1>Welcome to Fitness Trackr</h1>
+        <h2>Stay Consistent, Stay Hungry, and Build the Best Version of You!</h2>
+        <h3>If you are a new user, set up your account below</h3>
+
+
+        <button className='btn'>Register</button>
+        <h3>Already Have An Account?</h3>
+        <button className='btn'>Login</button>
+        <h3>Proceed As A Guest.</h3>
+        
+        <button className='btn'>View Activities</button>
+        
+        </div>
   
   
   </div>
