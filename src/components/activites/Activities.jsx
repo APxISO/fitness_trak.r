3089a98a43802454273dd7f003a605291a64dc88
Adam Perez <amperez0208@gmail.com>
2022-11-15 15:50:20 -0500

    activities api data now in grid

--- src/components/activites/Activities.jsx
+++ src/components/activites/Activities.jsx
@@ -1,56 +1,57 @@
-import React, { useState, useEffect } from 'react'
-// import axios from 'axios'
+import React, { useState, useEffect, Link } from 'react'
 import './activities.css'
 
-// import { useParams} from 'react-router-dom'
 const url = `https://fitnesstrac-kr.herokuapp.com/api/activities`;
 
 
-
-// {user}
-
-const Activities = () => {
+const Activities = ({routines, user, activites}) => {
     const [activities, setActivities] = useState([])
     const [data, setData] = useState(null);
     console.log('activities: ', activities)
     
     
     
-    useEffect(() => {
-        const fetchActivities = async () => {
-            const resp = await fetch (url);
-            const data = await resp.json();
-            setActivities(data);
-        }
-        fetchActivities();
-    }, [])
+useEffect(() => {
+    const fetchActivities = async () => {
+        const resp = await fetch (url);
+        const data = await resp.json();
+        setActivities(data);
+    }
+    fetchActivities();
+}, [])
 
-            return (
-        
-        <>
-        
-          <div className="activitiies_card">
-            <div className="activitiies_content">
-                <div className='activitiies_card_options'>
-                    <h1>Activities</h1>
-                    {
-                        activities.map(activities => <div key={activities.id}>
-                           <h3> {activities.name}</h3>
-                           <div>{activities.description}</div>
-                            </div>)
-                    }       
-                </div>
+
+    return (
+    
+    <div className='act_page_cont'>
+        <div className='act_top_cont'>
+            <div className='act_top_center'>
+                <h1>Activities</h1>
             </div>
+        {user ? (
+        <button className='btn'><Link to="/MyActivities">Create new activity</Link></button>
+        ) : (
+        <div className='act_top_right'>
+            <button className='btn'>Login to create activities</button>
         </div>
-        
-        </>
-      )};
+        )}
+        </div>
+
+        <div className='activities_container'>
+            {activities.map(activities => {
+                return (
+                    <div className='activities_card' key={activities.id} >
+                    <>
+                        <h3>{activities.name}</h3>
+                        <p>{activities.description}</p>
+                    </>
+                    </div>
+                )
+            })}
+        </div>
+</div>
+    )};
 
       
-
-
-
-
-
 
 export default Activities
